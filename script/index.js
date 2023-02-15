6d8ba866d8127b555ca2676a1c7c693fbd4aef9e
Severuna <oxxxyfomenkova@gmail.com>
2023-02-15 20:31:48 +0200

    create showBack func & call it

--- script/index.js
+++ script/index.js
@@ -3,5 +3,16 @@ const container = document.querySelector('.container');
 const backArr = [
     './images/banner_01.png',
     './images/banner_02.png',
-    './images/banner_01.png'
+    './images/banner_03.png'
 ]
+let i = 0;
+const showBack = () => {
+    i++
+    if(i > backArr.length - 1) {
+        i = 0
+    }
+    container.style.backgroundImage = `url('${backArr[i]}')`
+    setTimeout(showBack,2500)
+}
+
+showBack()
